b3bb8aa8dfb4ea87d994718301dc2497792549ef
ikon5508 <42330231+ikon5508@users.noreply.github.com>
2021-09-29 09:39:49 -0700

    Add files via upload

--- test.c
+++ test.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main ()
 {
+system ("date > test.txt");
 
-const char *str1 = "str1";
-const char *str2 = "str2";
-const char *str3 = "str3";
-const char *str4 = "str4";
-
-const char *point;
-
-point = str2;
-
-printf("this is a test; %s\n", point);
-
-
+return (0);
 }
