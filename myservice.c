173267a9d43ae8516a88d59de8ae13a38e8c468a
ikon5508 <42330231+ikon5508@users.noreply.github.com>
2021-09-29 19:55:20 -0700

    Add files via upload

---
+++ myservice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main () {
+
+while (1) {
+printf ("hello systemd \n");
+fflush (NULL);
+sleep (5);
+
+
+}
+return (EXIT_SUCCESS);
+
+}
